8916578a5355f0223d3cc79736f81520a8980a1e
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2018-05-11 03:25:03 -0300

    scripts js

---
+++ jscripts.js
@@ -0,0 +1,8 @@
+function toggler() {
+    var toggleThis = document.getElementById("hidden");
+    if (toggleThis.style.display == "none") {
+        toggleThis.style.display = "block";
+    } else{
+        toggleThis.style.display = "none";
+    }
+}
