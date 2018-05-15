7a05f74501c0861e76cc5e63a33945d880ce8627
matheusflausino <matheus.negocio@gmail.com>
2018-05-15 20:30:15 -0300

    Adição de scrollIntoView para os links ancora da página

--- matheus-flausino/assets/js/init.js
+++ matheus-flausino/assets/js/init.js
@@ -33,4 +33,14 @@ window.onload = function () {
     }
     });
 
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth', block:'start'
+            });
+        });
+    });
+
 }
