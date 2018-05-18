5f207fc095b349ff3a3c3dd387227c7a85e161d1
henriquecanella <henrique.mandelli@gmail.com>
2018-05-18 11:14:08 -0300

    Slider adicionado

---
+++ henrique-mandelli-canella/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+            // Init Slider
+            $('.slider').slider({height: 500, indicators: false});
+            // Init sidenav
+            $('.sidenav').sidenav();
+
+            });
+
