e46571dd9aca9813b4c1b5be209f44f694fb7dc8
Fábio Leonam <f.leonam@hotmail.com>
2018-05-18 14:53:20 -0300

    Inserção de comentários para melhor entendimento do código

--- Fabio_Leonam/js/script.js
+++ Fabio_Leonam/js/script.js
@@ -1,3 +1,5 @@
+
+//Inicio do código referente ao botão
 $botao = $('#back-to-top');
 
 $(document).ready(function() {
@@ -8,16 +10,24 @@ $(document).ready(function() {
 $(window).scroll(function() {
 
     console.log($(window).scrollTop());
-    if ($(window).scrollTop() > 100) {
-        $($botao).fadeIn();
+    if ($(window).scrollTop() > 100) { //Essa condição determina a partir de quantos pixels o botão aparecerá na tela
+        $($botao).fadeIn(); //FadeIn (botão aparece)
     } else {
-        $($botao).fadeOut();
+        $($botao).fadeOut(); //FadeOut (botão fica oculto)
     }
 });
 
 $botao.on("click", function(){	
-	$('html,body').animate({
+	$('html,body').animate({ //Animação para ser abragente em todo o site
 		scrollTop:0
-	}, 800);
+	}, 800); //Velocidade com que a página sobe ao topo, quanado o botão é clicado (800 milisegundos)
 });
+//Fim do código referente ao botão
 
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({
+        	scrollTop:$(this.hash).offset().top}, 2000);
+   });
+});
