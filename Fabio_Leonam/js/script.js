24c2704f909fa5efba15ec257f188188ccf5bc07
Fábio Leonam <f.leonam@hotmail.com>
2018-05-19 08:33:06 -0300

    Inserção de um scrollSuave entre o menu e o restante da página

--- Fabio_Leonam/js/script.js
+++ Fabio_Leonam/js/script.js
@@ -24,32 +24,12 @@ $botao.on("click", function(){
 });
 //Fim do código referente ao botão
 
-window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = 'BQA3YLIH7EYpefyp8-kbcU0U4i-dO0FwJQCljTfJb3RH9PC5NuGIwscen7eTH_1IR05RaWZSOXYZhlt1RqyR_Svrjc1G9_Y1mStUvMqRA8oCvjlZyIx2oTCp_xmBC9J-a7btiTN7bbhyQQOHNVK9pLemdWqQMdtwBz3G';
-  const player = new Spotify.Player({
-    name: 'Web Playback SDK Quick Start Player',
-    getOAuthToken: cb => { cb(token); }
-  });
 
-  // Error handling
-  player.addListener('initialization_error', ({ message }) => { console.error(message); });
-  player.addListener('authentication_error', ({ message }) => { console.error(message); });
-  player.addListener('account_error', ({ message }) => { console.error(message); });
-  player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-  // Playback status updates
-  player.addListener('player_state_changed', state => { console.log(state); });
-
-  // Ready
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id);
-  });
-
-  // Not Ready
-  player.addListener('not_ready', ({ device_id }) => {
-    console.log('Device ID has gone offline', device_id);
-  });
-
-  // Connect to the player!
-  player.connect();
-};
+//Animação de efeito deslizante 
+var $doc = $('html, body');
+$('.scrollSuave').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+    return false;
+});
