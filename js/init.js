d76c7621a730bd0a617631feb59ed18ea88f5af8
Davi Azevedo <daviaaze@gmail.com>
2018-05-20 02:23:30 -0300

    Acabei por recomeçar o esqueleto, pois cometi a bobagem de tentar fazer tudo de uma vez

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.sidenav').sidenav();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
