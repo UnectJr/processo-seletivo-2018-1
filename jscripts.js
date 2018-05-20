1d6b6d11cb25c1630901609d3d921c5ef7d1ec24
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2018-05-20 16:53:25 -0300

    Algumas funções de js vistas na oficina presentes em 'desafio.html'

--- jscripts.js
+++ jscripts.js
@@ -1,8 +1,18 @@
-function toggler() {
-    var toggleThis = document.getElementById("hidden");
-    if (toggleThis.style.display == "none") {
-        toggleThis.style.display = "block";
-    } else{
-        toggleThis.style.display = "none";
-    }
+var index = 1;
+function indexControl(n){
+	outputSlide(index +=n);
+}
+function outputSlide(n){
+	var i;
+	var slides= document.getElementsByClassName("slide");
+	if(n > slides.length){
+		index=1;
+	}
+	if(n < 1){
+		index=slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none"; 
+	}
+	slides[index-1].style.display = "block";
 }
