765fe43b1c9ce6c58284ab2bfefa1325365b2e53
Davi <daviaaze@gmail.com>
2018-05-20 15:23:35 -0300

    Fim!

--- js/init.js
+++ js/init.js
@@ -1,9 +1,11 @@
 (function($){
   $(function(){
 
+    $('.carousel').carousel();
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.tabs').tabs();
     $('.sidenav').sidenav();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
