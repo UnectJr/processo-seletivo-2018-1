97539f284a469b51ca3e370f585ba06f289edbe1
UTFPR <UTFPR>
2018-05-21 00:13:43 -0300

    Insercao do javascript e jquery

---
+++ Gabriel Yukio Takazaki/javascript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() { 
+    setTimeout(function() { 
+        $('#botao').removeClass("displayNone"); 
+ }, 4 * 1000); 
+});
+function wish() {
+    $('#frase').removeClass("displayNone");    
+    $('#imagem').removeClass("displayNone");
+}
