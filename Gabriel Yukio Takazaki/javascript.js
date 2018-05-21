8b8b86ab5cef3ee8a1e79caf2831b564307e28f3
UTFPR <UTFPR>
2018-05-21 01:48:23 -0300

    Slideshow adicionado

--- Gabriel Yukio Takazaki/javascript.js
+++ Gabriel Yukio Takazaki/javascript.js
@@ -1,9 +1,32 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
 $(document).ready(function() { 
     setTimeout(function() { 
-        $('#botao').removeClass("displayNone"); 
- }, 4 * 1000); 
+        $("#botao").removeClass("displayNone"); 
+    }, 4 * 1000); 
 });
+
 function wish() {
     $('#frase').removeClass("displayNone");    
     $('#imagem').removeClass("displayNone");
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = $(".slides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }    
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";
 }
