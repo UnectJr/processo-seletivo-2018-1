39e1cd3d15c6f24ac76b12df94bd779e3629a13b
Daniel Dantas <dantasmaarotti@gmail.com>
2018-05-21 20:22:54 -0300

    mudança BG/ ajuste width logo(navbar)/ add comentários

--- Daniel Dantas Marotti/js/main.js
+++ Daniel Dantas Marotti/js/main.js
@@ -1,15 +1,17 @@
+
+
 //inicializando firebase
- var config = {
+var config = {
     apiKey: "AIzaSyCohKVL1CUkZMy4cUO496zFVCbwFYNbcvM",
     authDomain: "contactform-portfolio.firebaseapp.com",
     databaseURL: "https://contactform-portfolio.firebaseio.com",
     projectId: "contactform-portfolio",
     storageBucket: "contactform-portfolio.appspot.com",
     messagingSenderId: "692556096608"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var messagesRef = firebase.database().ref('messages');
+var messagesRef = firebase.database().ref('messages');
 
 
 //pegando os valores submetidos
@@ -17,10 +19,10 @@ document.getElementById('form-contact').addEventListener('submit', submitForm);
 
 
 //submetendo o formulario
-function submitForm(e){
+function submitForm(e) {
     e.preventDefault();
 
-    var email = getInputVal('email');  
+    var email = getInputVal('email');
     var mensagem = getInputVal('mensagem');
 
 
@@ -31,10 +33,10 @@ function submitForm(e){
     document.querySelector('.alert').style.display = 'block';
 
     //fechando alerta apos 3s
-    setTimeout(function(){
+    setTimeout(function () {
         document.querySelector('.alert').style.display = 'none';
-    },3000);
-    
+    }, 3000);
+
 
     //limpando o formulario
     document.getElementById('form-contact').reset();
@@ -43,24 +45,24 @@ function submitForm(e){
 
 // função para pegar os valores do formulario
 
-function getInputVal(id){
+function getInputVal(id) {
     return document.getElementById(id).value;
 }
 
 
 //salvando as mensagens do firebase
-function saveMessage(email, mensagem){
+function saveMessage(email, mensagem) {
     var newMessageRef = messagesRef.push();
     newMessageRef.set({
         email: email,
         mensagem: mensagem
     });
 }
-
+//-----------------------------------------------------------------------------------------------------------------------------------------
 
 // Smooth Scrolling
 $(function () {
-    
+
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -75,11 +77,42 @@ $(function () {
     });
 });
 
+//ANIMACAO NO SCROLL (scrollReveal)
+window.sr = ScrollReveal();
+sr.reveal('.navbar-nav', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.container .figure', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.landing-text', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal('#sobre div', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.progress', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('.contact', {
+    duration: 2000,
+    origin: 'bottom'
+});
 
-$(function() {
-    
+
+
+//FUNCAO PARA DESABILITAR O NAVBAR-BRAND NA HOMEPAGE
+$(function () {
+
     var navbar = $(".navbar-null");
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
 
         if (scroll >= 700) {
@@ -90,32 +123,5 @@ $(function() {
     });
 });
 
-//ANIMACAO NO SCROLL (scrollReveal)
-window.sr = ScrollReveal();
-        sr.reveal('.navbar-nav', {
-            duration: 2000,
-            origin: 'bottom'
-        });
-        sr.reveal('.container .figure', {
-            duration: 2000,
-            origin: 'bottom'
-        });
-        sr.reveal('.landing-text', {
-            duration: 2000,
-            origin: 'top',
-            distance: '300px'
-        });
-        sr.reveal('#sobre div', {
-            duration: 2000,
-            origin: 'bottom'
-        });
-        sr.reveal('.progress', {
-            duration: 2000,
-            origin: 'left',
-            distance: '300px'
-        });    
-        sr.reveal('.contact', {
-            duration: 2000,
-            origin: 'bottom'
-        });      
-  
+
+
