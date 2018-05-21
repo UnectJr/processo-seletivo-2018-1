929604f199be558d05994d58e93a1d9b7e840008
henriquecanella <henrique.mandelli@gmail.com>
2018-05-21 11:56:57 -0300

    Adição do preloader e das postagens

--- henrique-mandelli-canella/script.js
+++ henrique-mandelli-canella/script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 
             // Init Slider
@@ -7,3 +9,9 @@ $(document).ready(function(){
 
             });
 
+//PRELOADER
+$(window).on('load', function () {
+    $('#preloader .inner').fadeOut(1500);
+    $('#preloader').delay(1500).fadeOut('slow'); 
+    $('body').delay(1500).css({'overflow': 'visible'});
+})
